e30c230f08803bef99fecb11558e48fee1cea41e
Thilina Ekanayaka <thilinaekanayaka@Thilinas-MacBook-Pro.local>
2024-11-30 21:53:37 +0800

    Server schema cleanup

--- server/src/schema.ts
+++ server/src/schema.ts
@@ -1,32 +1,32 @@
 import gql from "graphql-tag";
 
 const typeDefs = gql`
-
-  """
-  Sample query
-  """
   type Query {
+    "Get tracks array for homepage grid"
     tracksForHome: [Track!]!
   }
-
-  """
-  These are the tracks
-  """
+   
+  "A track is a group of Modules that teaches about a specific topic"
   type Track {
     id: ID!
+    "The track's title"
     title: String!
+    "The track's main author"
     author: Author!
+    "The track's main illustration to display in track card or track page detail"
     thumbnail: String
+    "The track's approximate length to complete, in minutes"
     length: Int
+    "The number of modules this track contains"
     modulesCount: Int
   }
-
-  """
-  These are the authors
-  """
+   
+  "Author of a complete Track"
   type Author {
     id: ID!
+    "Author's first and last name"
     name: String!
+    "Author's profile picture url"
     photo: String
   }
 `;
